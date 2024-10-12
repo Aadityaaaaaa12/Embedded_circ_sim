eeae526ee14a58e0667c41b9912ea000395c883f
Aadityaaaaaa12 <145829354+Aadityaaaaaa12@users.noreply.github.com>
2024-10-13 01:21:57 +0530

    Update Simulator.java

--- Simulator.java
+++ Simulator.java
@@ -9,11 +9,19 @@ public class Simulator {
 		CPU cpu = new CPU(memory,pins);
 		InstructionSet instructionSet = new InstructionSet(cpu, memory, pins);
 		Ground ground = new Ground();
-        LED led = new LED();
-        Resistor resistor = new Resistor();
+		seven_seg_display sseg = new seven_seg_display();
+		LED led_1 = new LED();
+        Resistor resistor_1 = new Resistor();
+		LED led_2 = new LED("blue");
+        Resistor resistor_2 = new Resistor(250);
 		
-		resistor.connect(pins.p1_1, ground);  
-        led.connect(pins.p1_0, pins.p1_1); 
+		resistor_1.connect(pins.p2_1, ground);  
+        led_1.connect(pins.p2_1,pins.p2_2);
+		
+		resistor_2.connect(pins.p2_3, ground);  
+        led_2.connect(pins.p2_3,pins.p2_4);
+		sseg.connect(pins.p2_0,pins.p1_0 ,pins.p1_1 ,pins.p1_2 ,pins.p1_3,pins.p1_4 ,pins.p1_5 ,pins.p1_6);
+		
 		
         // Load instructions from a file
         loadOpcodesFromFile(memory, "instructions.txt");
@@ -33,8 +41,12 @@ public class Simulator {
 
         cpu.displayRegisters();
         cpu.setAndDisplayPSW();
-		led.checkState();   
-        resistor.checkVoltageDrop();
+		led_1.checkState();   
+        resistor_1.checkVoltageDrop();
+		led_2.checkState();   
+        resistor_2.checkVoltageDrop();
+		sseg.run_ssd();
+		
     }
 	//loader function
     private static void loadOpcodesFromFile(Memory memory, String filePath) {
